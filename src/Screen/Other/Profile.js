543606975910707bb86402c914a2d0fd4c9e4b83
faiz164 <faiz.ahmed@deepmindz.co>
2022-10-31 12:23:39 +0530

    Profile page chnge

--- src/Screen/Other/Profile.js
+++ src/Screen/Other/Profile.js
@@ -6,21 +6,24 @@ import CommonText from '../../Component/Text/CommonText'
 import Textinput from '../../Component/Textinput/Textinput'
 import Button from '../../Component/Button/Button'
 import { useNavigation } from '@react-navigation/native'
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
-const Profile = ({route}) => {
-    const userData=route.params;
+const Profile = ({ route }) => {
+
+    const userData = route.params;
     const { phone_number: prefillPhone_number, email: prefillEMail } = route.params;
 
-    console.log(userData.userDetailsData.userDetails.first_name,'..............user')
+    console.log(userData.userDetailsData.userDetails.first_name, '..............user')
     const navigation = useNavigation()
     const scheme = useColorScheme()
-    
+
     const lazy_array = [
-        { name: 'First Name', value: "first_name" },
-        { name: 'Last Name', value: "last_name" },
-        { name: 'Email ID', value: "email_id" },
-        { name: 'Mobile Number', value: "mobile_number" },
-       
+        { name: 'First Name', value: "first_name", placeHolder: userData?.userDetailsData?.userDetails?.first_name },
+        { name: 'Last Name', value: "last_name", placeHolder: userData?.userDetailsData?.userDetails?.last_name },
+        { name: 'Email ID', value: "email_id", placeHolder: userData?.userDetailsData?.userDetails?.email },
+        { name: 'Mobile Number', value: "mobile_number", placeHolder: userData?.userDetailsData?.phone_number },
+
     ]
     const SignupSchema = Yup.object().shape({
         first_name: Yup.string()
@@ -33,42 +36,55 @@ const Profile = ({route}) => {
             .required('Last Name Required'),
 
     });
-    
+
     const handleSignup = async (values, event) => {
-  
+
     }
+
     return (
         <SafeAreaView style={[styles.container, { backgroundColor: scheme == 'dark' ? colors.backgroundDark : colors.backgroundLight }]}>
             <ScrollView>
                 <View style={styles.innerContainer}>
-                   {/* <Header /> */}
-                   <View style={styles.header}>
+                    {/* <Header /> */}
+                    <View style={styles.header}>
                         <BackButton />
                         <View style={styles.headerText}>
-                          <CommonText showText={'Profile'} fontSize={25} /> 
+                            <CommonText showText={'Profile'} fontSize={25} />
                         </View>
                     </View>
                     <View style={{ marginVertical: 20 }}>
                     </View>
-                    <View style={styles.textinputConatiner}>
-                       <CommonText showText={'First Name'} fontSize={12} />
-                        <Textinput />
-                    </View>
-                    <View style={styles.textinputConatiner}>
-                       <CommonText showText={'Last Name'} fontSize={12} /> 
-                        <Textinput />
-                    </View>
-                    <View style={styles.textinputConatiner}>
-                       <CommonText showText={'Email ID'} fontSize={12} /> 
-                        <Textinput />
-                    </View>
-                    <View style={styles.textinputConatiner}>
-                       <CommonText showText={'Mobile Number'} fontSize={12} /> 
-                        <Textinput />
-                    </View>
-                    <TouchableOpacity style={styles.button} >
-                        <Button  showText={'Save'} />
-                    </TouchableOpacity>
+
+                    <Formik
+                        initialValues={{
+                            first_name: '',
+                            last_name: "",
+
+                            email_id: prefillEMail || '',
+                            mobile_number: prefillPhone_number || ''
+                        }}
+                        onSubmit={handleSignup}
+                        validationSchema={SignupSchema}
+                    >
+                        {({ handleChange, handleBlur, handleSubmit, values, isSubmitting, errors, touched }) => (
+                            <>
+                                {lazy_array.map((e, i) => {
+                                    return (
+                                        <View key={i} style={styles.textinputConatiner}>
+                                            {scheme == 'dark' ? <CommonText showText={e.name} fontSize={14} /> : <CommonText showText={e.name} fontSize={14} />}
+                                            <Textinput value={values[e.value]} onChange={handleChange(e.value)} placeholderText={values[e.placeHolder]}  />
+                                            {errors[e.value] && touched[e.value] ? (
+                                                <CommonText showText={errors[e.value]} customstyles={{ color: colors.red }} fontSize={14} />
+                                            ) : null}
+                                        </View>
+                                    )
+                                })}
+                                <View style={styles.btnConatiner}>
+                                    <Button onPress={handleSubmit} showText={'Save'} onLoading={isSubmitting} />
+                                </View>
+                            </>
+                        )}
+                    </Formik>
                 </View>
             </ScrollView>
         </SafeAreaView>
