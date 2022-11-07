0bc0627621e6aab76c879c2c12e5f0e77acb6f52
Anj yadav <y.anuj98@gmail.com>
2022-11-07 11:41:48 +0530

    inset shadows

--- src/Component/Card/DenseCard/index.js
+++ src/Component/Card/DenseCard/index.js
@@ -2,21 +2,29 @@ import { useColorScheme, StyleSheet } from 'react-native'
 import React from 'react'
 import colors from '../../../Utils/colors'
 import LinearGradient from 'react-native-linear-gradient';
+import { Neomorph, Shadow, NeomorphFlex } from 'react-native-neomorph-shadows'
 
 export default function index({ children, style }) {
 
     const scheme = useColorScheme()
     return (
-        <LinearGradient colors={scheme === 'dark' ? [colors.denseShadowDark, colors.denseShadowSecondary] : [colors.denseShadow, colors.white]} style={[styles.linearGradient,style ]}>
+        <NeomorphFlex
+            inner // <- enable shadow inside of neomorph
+            swapShadows // <- change zIndex of each shadow color
+            style={{
+                shadowRadius: 3,
+                borderRadius: 12,
+                backgroundColor: colors.backgroundLight,
+                margin: 10,
+                marginVertical: 10,
+                padding: 15,
+            }}
+        >
             {children}
-        </LinearGradient >
+        </NeomorphFlex>
     )
 }
 
 const styles = StyleSheet.create({
-    linearGradient: {
-        padding: 10,
-        borderRadius: 10,
-        marginVertical: 10
-    },
+
 })
