bc64ca21fb1a6be3d175c58220b3770ce30535d7
bhagwasiyaritu <56601072+bhagwasiyaritu@users.noreply.github.com>
2022-11-16 16:23:51 +0530

    Header component change

--- src/Component/Button/BackButton.js
+++ src/Component/Button/BackButton.js
@@ -1,15 +1,13 @@
-import { View, Text, TouchableOpacity, StyleSheet, useColorScheme } from 'react-native'
+import { TouchableOpacity, useColorScheme } from 'react-native'
 import React from 'react'
 import colors from '../../Utils/colors'
 import AntDesign from 'react-native-vector-icons/AntDesign'
-import { useNavigation } from '@react-navigation/native'
 import CommonCard from '../../Component/Card/CommonCard/index'
-const BackButton = ({ onPress }) => {
-  const navigation = useNavigation()
-  const scheme = useColorScheme()
 
+const BackButton = ({ onPress }) => {
+  const scheme = useColorScheme()
   return (
-    <TouchableOpacity onPress={() => { navigation.goBack() }}>
+    <TouchableOpacity onPress={onPress}>
       <CommonCard margin={1}>
         <AntDesign name='left' size={25} color={scheme == 'dark' ? colors.white : colors.black} />
       </CommonCard>
