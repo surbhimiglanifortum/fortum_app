0b8b75eac29d8acf267a0384d6dfb1fb39ab11f8
bhagwasiyaritu <56601072+bhagwasiyaritu@users.noreply.github.com>
2022-11-22 17:10:28 +0530

    Bug Fix

--- src/Screen/Other/PaymentMethod/CompleteKYC.js
+++ src/Screen/Other/PaymentMethod/CompleteKYC.js
@@ -67,14 +67,6 @@ const CompleteKYC = ({ route }) => {
     const handleConfirm = (date) => {
          let dob = moment(date).format('LL')
         setDob(dob)
-        // let month = String(new Date(dob).getMonth() + 1)
-        // let day = String(new Date(dob).getDate())
-        // if (month.length <= 1)
-        //     month = '0' + month
-        // if (day.length <= 1)
-        //     day = '0' + day
-        // let fullDob = `${new Date(dob).getFullYear()}-${month}-${day}`
-        console.log(moment(date).format('YYYY-MM-DD'))
         setDobTech(moment(date).format('YYYY-MM-DD'))
         hideDatePicker();
     };
@@ -88,19 +80,8 @@ const CompleteKYC = ({ route }) => {
     };
 
     const docHandleConfirm = (date) => {
-
-
-
         let docDate = moment(date).format('LL')
         setDocDate(docDate)
-        // let month = String(new Date(docDate).getMonth() + 1)
-        // let day = String(new Date(docDate).getDate())
-        // if (month.length <= 1)
-        //     month = '0' + month
-        // if (day.length <= 1)
-        //     day = '0' + day
-        // let fullDocDate = `${new Date(docDate).getFullYear()}-${month}-${day}`
-        console.log(moment(date).format('YYYY-MM-DD'))
         setDocDateTech(moment(date).format('YYYY-MM-DD'))
         docHideDatePicker();
     };
