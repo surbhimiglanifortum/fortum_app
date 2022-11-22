59b0d5a41f958969e93fae177607b0b0f4b5f7fe
bhagwasiyaritu <56601072+bhagwasiyaritu@users.noreply.github.com>
2022-11-22 20:04:01 +0530

    Alignment Issue Resolved

--- src/Component/Button/TextButton.js
+++ src/Component/Button/TextButton.js
@@ -1,25 +1,25 @@
-import {  StyleSheet, TouchableOpacity, useColorScheme } from 'react-native'
+import { StyleSheet, TouchableOpacity, useColorScheme } from 'react-native'
 import React from 'react'
 import CommonText from '../Text/CommonText';
 
-const SkipButton = ({onPress}) => {
+const SkipButton = ({ onPress }) => {
 
   const scheme = useColorScheme();
 
 
   return (
     <TouchableOpacity style={styles.container} onPress={onPress}>
-     <CommonText showText={'Skip'} /> 
+      <CommonText showText={'Skip'} regular fontSize={14} />
     </TouchableOpacity>
   )
 }
 
-const styles=StyleSheet.create({
-    container:{
-paddingHorizontal:25,
-alignSelf:'flex-end',
-marginVertical:15
-    }
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 25,
+    alignSelf: 'flex-end',
+    marginVertical: 15
+  }
 })
 
 export default SkipButton
